6a0919258e6c351fcc3871d09771fb1a81c6b241
Shubham <samaroraytb@gmail.com>
2023-03-05 08:58:18 +0000

    React App

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,5 +1,35 @@
 import './index.css'
 
-const CommentItem = () => <h1>Hello</h1>
+const CommentItem = () => (
+  <li className="complete-comment-list-container">
+    <div className="dp-and-name-comment-section">
+      <p>R</p>
+      <div className="name-and-comment-sec">
+        <h1>Richard Branson</h1>
+        <p>
+          packages and web page editors now use Lorem Ipsum as their default //
+          eslint-disable-next-line react/no-unescaped-entities model text, and a
+          search for &apos;lorem ipsum&apos; will uncover many web sites still
+          in their infancy.
+        </p>
+      </div>
+    </div>
+    <div className="like-and-delete-button">
+      <button type="button" className="like-unlike">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png"
+          alt="like unlike"
+        />
+      </button>
+      <button type="button" className="like-unlike">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+          alt="delete"
+        />
+      </button>
+    </div>
+    <hr className="horizontal-line-style" />
+  </li>
+)
 
 export default CommentItem
--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -5,6 +5,7 @@ import './index.css'
 import {v4 as uuidv4} from 'uuid'
 // eslint-disable-next-line no-unused-vars
 import {formatDistanceToNow} from 'date-fns'
+import CommentItem from '../CommentItem'
 
 const initialContainerBackgroundClassNames = [
   'amber',
@@ -22,27 +23,40 @@ class Comments extends Component {
   render() {
     return (
       <div className="main-container">
+        <h1 className="comment-main-heading">Comments</h1>
         <div className="input-content-container-with-image">
           <img
+            className="main-comment-image"
             src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
             alt="comments"
           />
           <div className="comment-input-section">
-            <p>Say something about 4.0 Technologies</p>
-            <input type="text" placeholder="Your Name" />
+            <label htmlFor="yourName" className="comment-section-paragraph">
+              Say something about 4.0 Technologies
+            </label>
+            <input
+              className="input-style"
+              id="yourName"
+              type="text"
+              placeholder="Your Name"
+            />
             <br />
-            <textarea placeholder="Your comment" />
-            <button type="button">Add Comment</button>
+            <textarea
+              className="input-style text-area-input"
+              placeholder="Your comment"
+            />
+            <button className="add-comment-button" type="button">
+              Add Comment
+            </button>
           </div>
         </div>
-        <hr />
+        <hr className="horizontal-line-style" />
         <div className="show-comment-list-section">
-          <div className="comment-count-section">
-            <p>0</p>
-            <p>Comments</p>
-          </div>
+          <p className="comment-count">
+            <span className="comment-count-number">0</span> Comments
+          </p>
           <ul className="list-of-comments-container">
-            <li>Hello</li>
+            <CommentItem />
           </ul>
         </div>
       </div>
