74cdad8ce39e5389f04c047792ae4f50286071cd
Shubham <samaroraytb@gmail.com>
2023-03-05 09:52:42 +0000

    React App design

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -3,10 +3,12 @@ import './index.css'
 const CommentItem = () => (
   <li className="complete-comment-list-container">
     <div className="dp-and-name-comment-section">
-      <p>R</p>
+      <div className="dp-section">
+        <p>R</p>
+      </div>
       <div className="name-and-comment-sec">
-        <h1>Richard Branson</h1>
-        <p>
+        <p className="commenter-name-style">Richard Branson</p>
+        <p className="commenter-comment-style">
           packages and web page editors now use Lorem Ipsum as their default //
           eslint-disable-next-line react/no-unescaped-entities model text, and a
           search for &apos;lorem ipsum&apos; will uncover many web sites still
@@ -15,14 +17,17 @@ const CommentItem = () => (
       </div>
     </div>
     <div className="like-and-delete-button">
-      <button type="button" className="like-unlike">
+      <button type="button" className="common-button like-unlike-button">
         <img
+          className="image-width-for-like-and-delete"
           src="https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png"
           alt="like unlike"
         />
+        <p className="like-text-nochange">Like</p>
       </button>
-      <button type="button" className="like-unlike">
+      <button type="button" className="common-button">
         <img
+          className="image-width-for-like-and-delete"
           src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
           alt="delete"
         />
